86fe71f58ab21d2334a7ba31a0ebff633f80849a
Chibu <chibueze.k.muoneke@gmail.com>
2024-04-18 12:18:40 +0100

    finished the bare basic UI, set all required state and deleted unwanted files from the file tree. Next is putting in the API calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [amount,setAmount] = useState('');
+  const [currency1,setCurrency1] = useState(0)
+  const [currency2, setCurrency2] = useState(0)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <input 
+      type='number' 
+      value={amount} 
+      onChange={(e)=>setAmount(e.target.value)}  
+      placeholder='type in the currency'/>
+            
+            <select onChange={(e)=> setCurrency1(e.target.value)} value={currency1}>
+             <option value='USD'>USD</option>
+             <option value='EUR'>EUR</option>
+             <option value='NGN'>NGN</option>
+
+            </select>
+            
+            <select value={currency2} onChange={(e)=> setCurrency2(e.target.value)}>
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+              <option value="NGN">NGN</option>
+            </select>
+
     </div>
   );
 }
