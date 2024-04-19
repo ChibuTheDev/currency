940aab72e5e420d17363e64c4c47e3b3970b3dea
Chibu <chibueze.k.muoneke@gmail.com>
2024-04-19 15:43:16 +0100

    finished the main functionality, fetching and displaying the currencies exchange rate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [amount,setAmount] = useState('');
-  const [currency1,setCurrency1] = useState(0)
-  const [currency2, setCurrency2] = useState(0)
+  const [amount,setAmount] = useState(1);
+  const [currency1,setCurrency1] = useState('USD');
+  const [currency2, setCurrency2] = useState('EUR');
+  const [newCurr, setNewCurr] = useState(0)
 
+  const host = 'api.frankfurter.app'
 
+    useEffect(function(){
+        
+        async function convertCurr(){
+
+          if (!amount || !currency1 || !currency2) {
+            console.log('Please provide valid amount and currencies');
+            return;
+          }
+
+          const res = await fetch(`https://${host}/latest?amount=${amount}&from=${currency1}&to=${currency2}`);
+          const data= await res.json()
+       
+          setNewCurr(data.rates[currency2])
+            
+        }
+           convertCurr();
+
+    },[amount,currency1,currency2])
 
   return (
     <div>
@@ -27,12 +47,12 @@ function App() {
             </select>
             
             <select value={currency2} onChange={(e)=> setCurrency2(e.target.value)}>
-              <option value="USD">USD</option>
               <option value="EUR">EUR</option>
-              <option value="NGN">NGN</option>
+              <option value="GBP">GBP</option>
+              <option value="JPY">JPY</option>
             </select>
 
-            <p>Output is :</p>
+            <p>Output is : {newCurr} {currency2} </p>
 
     </div>
   );
